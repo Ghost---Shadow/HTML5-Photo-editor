aaab320d7ff2d85d019fc5186e4fda72fe81583a
Ghost---Shadow <super.sushi269@gmail.com>
2016-03-06 18:17:53 +0530

    Added drag and drop feature

--- scripts/functions.js
+++ scripts/functions.js
@@ -45,6 +45,28 @@ $(document).ready(function() {
         W = img.width;
         H = img.height;  
     }
+
+    // To enable drag and drop
+    canvas.addEventListener("dragover", function (evt) {
+        evt.preventDefault();
+    }, false);
+
+    // Handle dropped image file - only Firefox and Google Chrome
+    canvas.addEventListener("drop", function (evt) {
+        var files = evt.dataTransfer.files;
+        if (files.length > 0) {
+            var file = files[0];
+            if (typeof FileReader !== "undefined" && file.type.indexOf("image") != -1) {
+                var reader = new FileReader();
+                // Note: addEventListener doesn't work in Google Chrome for this event
+                reader.onload = function (evt) {
+                    img.src = evt.target.result;
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+        evt.preventDefault();
+    }, false);   
       
     
     $("#bBlur").mousedown(function() {
